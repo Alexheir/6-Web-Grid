4841b2ae742fe6093a4f80292850a6cef48e2849
Román De la Cruz <160436137+Alexheir@users.noreply.github.com>
2024-02-23 03:15:42 -0500

    botones intento

--- js/main.js
+++ js/main.js
@@ -41,3 +41,54 @@ function loadPhotos() {
 }
 
 loadPhotos();
+
+// Seleccionar todos los botones y el botón activo
+let btnAll = document.querySelectorAll(".btn");
+let btnActive = document.querySelector(".btn.active");
+
+// Función para mostrar/ocultar los botones
+function display() {
+  btnAll.forEach((element) => {
+    if (!element.classList.contains("display")) {
+      element.classList.toggle("hidden");
+    }
+  });
+}
+
+// Agregar evento click al botón activo para mostrar/ocultar los botones
+btnActive.addEventListener("click", display);
+
+// Función para manejar el click en los botones
+function btnClick(e) {
+  const target = e.target;
+
+  // Si el elemento no tiene clase, intenta obtener la clase del padre
+  let className = target.classList.length > 0 ? target.classList[0] : "";
+  if (className.trim() === "") {
+    const parentClassName = target.parentNode.className;
+    if (parentClassName.trim() !== "") {
+      className = parentClassName;
+    } else {
+      className = "sin-clase";
+    }
+  }
+
+  // Si el elemento clicado es un botón
+  if (target.classList.contains("btn") || target.classList.contains("")) {
+    // Remover la clase "active" de todos los botones y agregarla al clicado
+    btnAll.forEach((e) => e.classList.remove("active"));
+    target.classList.add("active");
+    // Mover elemento al primer puesto dentro de su contenedor
+    const parentContainer = target.parentNode;
+    parentContainer.prepend(target);
+    if(!target.classList.contains("active")) {
+      // Remover la clase "active" de todos los botones y agregarla al clicado
+      btnAll.forEach((e) => e.classList.add("hidden"))}
+  }
+  console.log(className);
+}
+
+// Agregar evento click a todos los botones
+btnAll.forEach(function (e) {
+  e.addEventListener("click", btnClick);
+});
