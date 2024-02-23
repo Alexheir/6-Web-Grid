3918d7c878901cfbef4140c79f3636cfb0d63b9d
Román De la Cruz <160436137+Alexheir@users.noreply.github.com>
2024-02-22 21:06:36 -0500

    photos-2

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ const photoContainer = document.getElementById("photo-container");
 async function renderPhotoElement(photo_id) {
   try {
     const photoElementHTML = await getPhotoElement(photo_id);
-    photoContainer.innerHTML += photoElementHTML;
+    return photoElementHTML; // Devuelve el HTML del elemento de la foto
   } catch (error) {
     console.error("Error al renderizar la foto:", error);
+    return ""; // Devuelve una cadena vacía en caso de error
   }
 }
-
 async function getPhotoElement(photo_id) {
   var jpgExists = await checkFileExists(`./img/photo_${photo_id}.jpg`);
   var jpegExists = await checkFileExists(`./img/photo_${photo_id}.jpeg`);
@@ -34,10 +34,29 @@ async function checkFileExists(url) {
   }
 }
 
-function loadPhotos() {
-  for (let i = 0; i <= 10; i++) {
-    renderPhotoElement(i);
+async function loadPhotos() {
+  let content = `<div class="column">`; // Inicializa content dentro de loadPhotos
+
+  for (let i = 0; i < 20; i++) {
+    if (i !== 0 && i % 5 === 0) {
+      content += `</div><div class="column">`;
+    }
+
+    try {
+      // Espera a que se complete la carga de la imagen
+      const photoElementHTML = await renderPhotoElement(i + 1);
+      // Agrega el HTML de la imagen al contenido
+      content += photoElementHTML;
+    } catch (error) {
+      console.error("Error al cargar la foto:", error);
+    }
   }
+
+  // Cierra la última columna después de insertar todas las imágenes
+  content += `</div>`;
+
+  // Inserta el contenido HTML en el contenedor
+  photoContainer.innerHTML = content;
 }
 
 loadPhotos();
