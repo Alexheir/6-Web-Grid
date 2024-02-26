157c3ea9e2706042af6f024b61779e0a7cb0e5d3
Román De la Cruz <160436137+Alexheir@users.noreply.github.com>
2024-02-26 15:52:32 -0500

    sidebar 2 problema resuelto

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,15 +1,15 @@
-let allBtns = document.querySelectorAll(".btn");
+let headerAllBtns = document.querySelectorAll("aside .btn");
 
 function btnToggle(e) {
-  allBtns.forEach(function (btn) {
+  headerAllBtns.forEach(function (btn) {
     btn.classList.remove("active");
   });
   // Agregar la clase 'active' al elemento <li> padre del <a> clickeado
   e.target.parentElement.classList.add("active");
 }
 
-allBtns.forEach(function (btn) {
+headerAllBtns.forEach(function (btn) {
   btn.querySelector("a").addEventListener("click", btnToggle);
 });
 
-allBtns[0].classList.add("active")
+headerAllBtns[0].classList.add("active");
