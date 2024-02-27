64e980d09e642271965e33a93c8d510823f9f0fb
Román De la Cruz <160436137+Alexheir@users.noreply.github.com>
2024-02-27 17:28:33 -0500

    mejora script aside

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,50 +1,33 @@
-let headerAllBtns = document.querySelectorAll("aside .btn");
-
-function btnToggle(e) {
-  headerAllBtns.forEach(function (btn) {
-    btn.classList.remove("active");
-  });
-  // Agregar la clase 'active' al elemento <li> padre del <a> clickeado
-  e.target.parentElement.classList.add("active");
-}
-
-headerAllBtns.forEach(function (btn) {
-  btn.querySelector("a").addEventListener("click", btnToggle);
-});
-
-headerAllBtns[0].classList.add("active");
-
-async function executeAfterImagesLoaded() {
-  // Esperar a que se carguen las imágenes
+async function postImageLoad() {
   await new Promise((resolve) => {
     window.addEventListener("load", resolve);
   });
 
-  // Una vez que se cargan las imágenes, ejecutar el código
-  headerAllBtns.forEach(function (e) {
-    e.addEventListener("click", function () {
-      // Remover la clase 'active' de todos los botones
-      headerAllBtns.forEach(function (btn) {
-        btn.classList.remove("active");
-      });
-      
-      // Agregar la clase 'active' al botón clickeado
-      e.classList.add("active");
-      
-      // Obtener el tag name del botón clickeado
-      let tagName = e.querySelector("a").innerText.trim();
-      
-      // Filtrar los divs según el tag name
-      let photoDivs = document.querySelectorAll("#photo-container .photo");
-      photoDivs.forEach(function (div) {
-        if (tagName === "All" || div.dataset.tags.includes(tagName)) {
-          div.style.display = "block";
-        } else {
-          div.style.display = "none";
-        }
-      });
+  let asideBtns = document.querySelectorAll("aside .btn");
+  let photoDivs = document.querySelectorAll("#photo-container .photo");
+
+  function btnToggle(e) {
+    asideBtns.forEach(function (btn) {
+      btn.classList.remove("active");
     });
+    e.currentTarget.parentElement.classList.add("active");
+
+    //Captura de tag desde el nombre del btn
+    let textBtn = e.currentTarget.innerText.trim();
+
+    //Filtro de imagenes según Data-Tag
+    photoDivs.forEach(function (div) {
+      if (textBtn === "All" || div.dataset.tags.includes(textBtn)) {
+        div.style.display = "block";
+      } else {
+        div.style.display = "none";
+      }
+    });
+  }
+
+  asideBtns.forEach(function (e) {
+    e.addEventListener("click", btnToggle);
   });
 }
-executeAfterImagesLoaded();
 
+postImageLoad();
